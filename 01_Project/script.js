3073436cd774b9483512a8f446096ec4b9f586ab
Yokesh-Dev <yokeshramana21@gmail.com>
2025-05-22 14:26:29 +0530

    project 1

---
+++ 01_Project/script.js
@@ -0,0 +1,15 @@
+//Created a Varible to call class panel
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
